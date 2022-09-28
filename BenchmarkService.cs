532314abbabcc4884f4c00afe71ffb8fd2d5eebd
Aghogho Bernard <aghoghobernard@gmail.com>
2022-09-28 22:10:24 +0400

    optimized authors

--- BenchmarkService.cs
+++ BenchmarkService.cs
@@ -88,9 +88,48 @@ namespace OptimizeMePlease
         [Benchmark]
         public List<AuthorDTO> GetAuthors_Optimized()
         {
-            List<AuthorDTO> authors = new List<AuthorDTO>();
+            using var dbContext = new AppDbContext();
+            var authors = dbContext.Authors
+                                        .AsNoTracking()
+                                        .Include(x => x.User)
+                                        .ThenInclude(x => x.UserRoles)
+                                        .ThenInclude(x => x.Role)
+                                        .Include(x => x.Books)
+                                        .ThenInclude(x => x.Publisher)
+                                        .Where(x => x.Country == "Serbia" && x.Age == 27)
+                                        .OrderByDescending(x => x.BooksCount)
+                                        .Select(x => new AuthorDTO
+                                        {
+                                            UserCreated = x.User.Created,
+                                            UserEmailConfirmed = x.User.EmailConfirmed,
+                                            UserFirstName = x.User.FirstName,
+                                            UserLastActivity = x.User.LastActivity,
+                                            UserLastName = x.User.LastName,
+                                            UserEmail = x.User.Email,
+                                            UserName = x.User.UserName,
+                                            UserId = x.User.Id,
+                                            RoleId = x.User.UserRoles.FirstOrDefault(y => y.UserId == x.UserId).RoleId,
+                                            BooksCount = x.BooksCount,
+                                            AllBooks = x.Books.Select(y => new BookDto
+                                            {
+                                                Id = y.Id,
+                                                Name = y.Name,
+                                                Published = y.Published,
+                                                ISBN = y.ISBN,
+                                                PublisherName = y.Publisher.Name,
+                                                PublishedYear = y.Published.Year < 1900 ? y.Published.Year : 0
+                                            }).ToList(),
+                                            AuthorAge = x.Age,
+                                            AuthorCountry = x.Country,
+                                            AuthorNickName = x.NickName,
+                                            Id = x.Id
+                                        }).Take(2).ToList();
+            
 
             return authors;
+            //List<AuthorDTO> authors = new List<AuthorDTO>();
+
+            //return authors;
         }
     }
 }
