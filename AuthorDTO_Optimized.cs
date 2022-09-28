791a0176fde38027eb782d96951df42103cb11a0
MuhammedMajid <mmajid@diligend.com>
2022-09-28 13:44:59 +0400

    Muhammed Majid Optimization

---
+++ AuthorDTO_Optimized.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace OptimizeMePlease
+{
+    public class AuthorDTO_Optimized
+    {
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public int AuthorAge { get; set; }
+        public string AuthorCountry { get; set; }
+        public IEnumerable<BookDto_Optimized> AllBooks { get; set; }
+    }
+}
