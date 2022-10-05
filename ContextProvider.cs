89ad01395f8f61f352bdbce09d20e1ff90272457
rudis <rudi.steinbach@gmail.com>
2022-10-05 12:51:57 +0200

    Add context static hack disclaimer...

--- ContextProvider.cs
+++ ContextProvider.cs
@@ -3,6 +3,7 @@ using OptimizeMePlease.Context;
 
 namespace OptimizeMePlease
 {
+    // I use a static context to gain a few more ms, must not be used in code meant for production.
     public static class ContextProvider
     {
         static ContextProvider()
